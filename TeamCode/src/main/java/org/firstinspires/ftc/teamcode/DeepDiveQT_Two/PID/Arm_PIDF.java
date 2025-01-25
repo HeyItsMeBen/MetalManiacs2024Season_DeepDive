d280f6aa6030b385d1bd62dca64c192ab83c7f2a
FriendOfTheFire <andrewscientist09@gmail.com>
2025-01-25 12:06:13 -0800

    Update Arm_PIDF.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeepDiveQT_Two/PID/Arm_PIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeepDiveQT_Two/PID/Arm_PIDF.java
@@ -4,33 +4,43 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-@Disabled
+
+
 @Config
 @TeleOp
 public class Arm_PIDF extends OpMode {
     private PIDController controller;
 
-    public static double p = 0, i = 0, d = 0;
-    public static double f = 0;
+
+    public static double p = 0.00575, i = 0.1, d = 0.0005;
+    public static double f = 0.05;
 
 
-    public static int target = 0;   //0-->1
+    public static int target = 50;   //0-->1
+
 
     private final double ticks_in_degree = 700 / 180.0;
 
+
     private DcMotorEx arm_motor;
+
 
     @Override
     public void init() {
         controller = new PIDController(p, i, d);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
+
         arm_motor = hardwareMap.get(DcMotorEx.class, "arm");
+        arm_motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        arm_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm_motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
     }
+
 
     @Override
     public void loop() {
@@ -39,9 +49,12 @@ public class Arm_PIDF extends OpMode {
         double pid = controller.calculate(armPos, target);
         double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
 
+
         double power = pid + ff;
 
+
         arm_motor.setPower(power);
+
 
         telemetry.addData("pos", armPos);
         telemetry.addData("target", target);
